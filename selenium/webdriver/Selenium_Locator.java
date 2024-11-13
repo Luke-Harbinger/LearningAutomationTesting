6cd5128a9e80641050930a506da636d6cdf7643d
luhieu <luhieu2412@gmail.com>
2024-11-13 17:15:50 +0700

    Add Selenium_Locator & Template

---
+++ selenium/webdriver/Selenium_Locator.java
@@ -0,0 +1,39 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Selenium_Locator {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/register");
+
+    }
+//TestNG:Order Testcase by Alphabet
+    @Test
+    public void TC_01_ID() {
+        driver.findElement(By.id("FirstName")).sendKeys("Luke");
+    }
+
+    @Test
+    public void TC_02_Class() {
+        driver.findElement(By.className("header-logo"));
+    }
+}
