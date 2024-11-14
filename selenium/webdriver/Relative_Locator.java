080b52a1fda273663b5e253843e3a669b1076509
luhieu <luhieu2412@gmail.com>
2024-11-14 15:43:02 +0700

    Learning Relative Locator

---
+++ selenium/webdriver/Relative_Locator.java
@@ -0,0 +1,56 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Relative_Locator {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+
+
+    }
+
+    @Test
+    public void TC_01() {
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2Fregister");
+
+        By loginButtonBy = By.cssSelector("button.login-button"); // Define Login button
+        By remembermeCheckboxBy = By.id("RememberMe"); // Define Rememberme checkbox
+        WebElement forgotPasswordElement = driver.findElement(By.className("forgot-password")); // Define forgotPassword Textlink
+        WebElement passwordTextboxElement = driver.findElement(By.id("Password")); // Define passwordTextbox
+        //Define Element Rememberme Text by RelativeLocator | Can check GUI (location / position)
+        WebElement remembermeTextElement = driver.findElement(RelativeLocator.with(By.tagName("label"))
+                .above(loginButtonBy)
+                .toRightOf(remembermeCheckboxBy)
+                .toLeftOf(forgotPasswordElement)
+                .below(passwordTextboxElement)
+        );
+        System.out.println(remembermeTextElement.getText());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
--- selenium/webdriver/Template_Selenium.java
+++ selenium/webdriver/Template_Selenium.java
@@ -3,6 +3,7 @@ package webdriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -23,12 +24,17 @@ public class Template_Selenium {
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+
 
     }
 
     @Test
     public void TC_01() {
 
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
     }
 }
