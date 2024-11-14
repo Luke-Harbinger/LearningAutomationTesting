8f6cbd4a4cb17bee2376901fa004b3b528cd9974
luhieu <luhieu2412@gmail.com>
2024-11-14 11:28:58 +0700

    Update code Selenium_locator

--- selenium/webdriver/Selenium_Locator.java
+++ selenium/webdriver/Selenium_Locator.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
 
 public class Selenium_Locator {
@@ -58,4 +59,43 @@ public class Selenium_Locator {
         driver.findElement(By.partialLinkText("for vendor"));
         driver.findElement(By.partialLinkText("vendor account"));
     }
+
+    @Test
+    public void TC_07_Css() {
+        //ID
+        driver.findElement(By.cssSelector("input[id='FirstName']"));
+        driver.findElement(By.cssSelector("input#FirstName"));
+        driver.findElement(By.cssSelector("#FirstName"));
+        //Class
+        driver.findElement(By.cssSelector("div[class='page-title']"));
+        driver.findElement(By.cssSelector("div.page-title"));
+        driver.findElement(By.cssSelector(".page-title"));
+        //Name
+        driver.findElement(By.cssSelector("input[name='FirstName']"));
+        //Tagname
+        driver.findElement(By.cssSelector("input"));
+        //LinkText
+        driver.findElement((By.cssSelector("a[href='/customer/addresses']")));
+        //Partial LinkText
+        driver.findElement((By.cssSelector("a[href*='addresses']")));
+    }
+
+    @Test
+    public void TC_08_XPath() {
+        //ID
+        driver.findElement(By.xpath("//input[@id='FirstName']"));
+        //Class
+        driver.findElement(By.xpath("//div[@class='page-title']"));
+        //Name
+        driver.findElement(By.xpath("//input[@name='FirstName']"));
+        //Tagname
+        driver.findElement(By.xpath("//input"));
+        //LinkText
+        driver.findElement((By.xpath("//a[@href='/customer/addresses']")));
+        driver.findElement((By.xpath("//a[text()='Addresses']")));
+        //Partial LinkText
+        driver.findElement((By.xpath("//a[contains(@href,'addresses')]")));
+        driver.findElement((By.xpath("//a[contains(text(),'Addresses')]")));
+    }
+
 }
